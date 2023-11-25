c58d856f17960f5b156b3f144a8082208f206215
STRiiXER <127283772+striixervro@users.noreply.github.com>
2023-11-25 09:21:23 +0530

    first commit

---
+++ 02-DataTypes&EcmaStanderds.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all js code as newer version
+//alert(3+3) //  shows alert popup msg
+
+// Datatypes 
+
+let name = "xyz"
+let age = 21
+let isLoggedIn = false
+let state;
+//number,  bigint, string, boolean, null (standalone value), undefined, symbol (to find uniqueness)
+
+// Object
+console.log(typeof"XYZ", age, isLoggedIn, state)
