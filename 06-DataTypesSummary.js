210cf078dd57d6fc16eb95f330bec69acacfea09
STRiiXER <127283772+striixervro@users.noreply.github.com>
2023-11-25 15:05:05 +0530

    lec5

---
+++ 06-DataTypesSummary.js
@@ -0,0 +1,29 @@
+//  Primitive datatypes
+//  7 Types : String, Number, boolean, null, undefined, Symbol (uniqueness), BigInt
+
+const score = 100 
+const scoreValue = 100.3
+const isLoggedIn = false
+const outSideTemp = null 
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123') 
+console.log(id === anotherId); // not same it is unique 
+
+const bigNumber = 4753987593759375983475934985739595737534789573n  //bigint
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name:"abc",
+    age : 21
+}
+
+const myFunc = function() {
+    console.log("hello world!");
+}
