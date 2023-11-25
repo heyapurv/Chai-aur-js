7780063aca47ac9ab1f1d41a823229e0cee29e26
STRiiXER <127283772+striixervro@users.noreply.github.com>
2023-11-25 09:47:06 +0530

    Lec3

---
+++ 04-DataTypeConversionOperations.js
@@ -0,0 +1,31 @@
+let value = 3 
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2); => used in cryptography, algorithms
+
+let str1 = "Hello" 
+let str2 = " there!"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+1);
+console.log(1+"3");
+console.log("1"+"3");
+console.log("1"+2+2);
+console.log(2+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;  // refer prefix and postfix concept on mdn docs
+console.log(gameCounter);
